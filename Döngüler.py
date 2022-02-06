650f17bb3f7455d24025dd002001d6203977cfb4
Kaan Olcay <akcolcay@hotmail.com>
2022-02-06 12:41:31 +0300

    Genel

---
+++ Döngüler.py
@@ -0,0 +1,20 @@
+#döngüer
+krediler =["Hızlı Kredi","Maaşını Halkbank tan alanlara özel","Mutlu emekli ihtiyaç kredisi"]
+
+
+for kredi in krediler:
+    print(kredi)
+
+for i in range(len(krediler)): #krediler içindeki listenin elemanlarını yazar
+    print(krediler[i])
+
+for i in range(10): #0'dan 10 a yazar
+    print(i)
+
+for i in range(3,10): #3'ten 10 a yazar
+    print(i)
+    
+for i in range(0,10,2): #0'dan 10 a kadar 2 şer arttır
+    print(i)
+    
+    
---
+++ Fonksiyonlar.py
@@ -0,0 +1,11 @@
+#fonksiyonlar
+def kredileriListele():
+    krediler =["Hızlı Kredi","Maaşını Halkbank tan alanlara özel","Mutlu emekli ihtiyaç kredisi"]
+    for kredi in krediler:
+        print(kredi)
+
+def main():
+    kredileriListele()
+    print("Selam Dünya")
+    
+main()    
